02779caf1d067f70a2ec3258f6594eb4ef25bb7d
wb <w.bosma@nfi.nl>
2023-11-08 13:02:07 +0100

    .

--- tests/context.py
+++
@@ -1,9 +0,0 @@
-from __future__ import absolute_import
-
-import os
-import sys
-
-# this is a hack https://github.com/pytest-dev/pytest/issues/2421
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
-import lir

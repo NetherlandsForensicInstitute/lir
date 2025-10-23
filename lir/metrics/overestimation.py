7e7bbede3fb310556efa1c1d8c83c9be7c51e791
wb <20113294+wowtor@users.noreply.github.com>
2025-10-23 13:34:52 +0200

    implemented LLR overestimation metric and plotting function

--- lir/metrics/overestimation.py
+++ lir/metrics/overestimation.py
@@ -11,8 +11,8 @@ def llr_overestimation(llrs: np.ndarray, y: np.ndarray, **kwargs: Any) -> float:
     Calculates the mean absolute value of the LLR-overestimation.
     """
     _, llr_overestimation_grid, _ = calc_llr_overestimation(
-        llrs, y, num_fids=0, **kwargs
+        logodds_to_odds(llrs), y, num_fids=0, **kwargs
     )
     return (
-        np.mean(np.abs(llr_overestimation_grid)) if llr_overestimation_grid.all() else np.nan
+        np.mean(np.abs(llr_overestimation_grid)) if llr_overestimation_grid else np.nan
     )
--- tests/algorithms/llr_overestimation.py
+++ tests/algorithms/llr_overestimation.py
@@ -1,4 +1,5 @@
 import numpy as np
+import unittest
 
 from lir.algorithms.llr_overestimation import calc_llr_overestimation
 from lir.metrics.overestimation import llr_overestimation
@@ -13,33 +14,33 @@ def test_llr_overestimation_metric():
     rng = np.random.default_rng(42)
     lr_h1 = np.exp(rng.normal(+lnlr_mean, lnlr_sigma, 100))
     lr_h2 = np.exp(rng.normal(-lnlr_mean, lnlr_sigma, 100))
-    llrs = np.log10(np.concatenate((lr_h1, lr_h2)))
+    lrs = np.concatenate((lr_h1, lr_h2))
     y = np.concatenate((np.ones(len(lr_h1)), np.zeros(len(lr_h2))))
-    np.testing.assert_almost_equal(0.2473928, llr_overestimation(llrs, y, seed=42))
+    np.testing.assert_almost_equal(0.2473928, llr_overestimation(lrs, y, seed=42))
 
     # big perfect system
     rng = np.random.default_rng(42)
     lr_h1 = np.exp(rng.normal(+lnlr_mean, lnlr_sigma, 1000))
     lr_h2 = np.exp(rng.normal(-lnlr_mean, lnlr_sigma, 1000))
-    llrs = np.log10(np.concatenate((lr_h1, lr_h2)))
+    lrs = np.concatenate((lr_h1, lr_h2))
     y = np.concatenate((np.ones(len(lr_h1)), np.zeros(len(lr_h2))))
-    np.testing.assert_almost_equal(0.0736284, llr_overestimation(llrs, y, seed=42))
+    np.testing.assert_almost_equal(0.0736284, llr_overestimation(lrs, y, seed=42))
 
     # big shifted system
     rng = np.random.default_rng(42)
     lr_h1 = np.exp(rng.normal(+lnlr_mean, lnlr_sigma, 1000) + np.log(10 ** 1))
     lr_h2 = np.exp(rng.normal(-lnlr_mean, lnlr_sigma, 1000) + np.log(10 ** 1))
-    llrs = np.log10(np.concatenate((lr_h1, lr_h2)))
+    lrs = np.concatenate((lr_h1, lr_h2))
     y = np.concatenate((np.ones(len(lr_h1)), np.zeros(len(lr_h2))))
-    np.testing.assert_almost_equal(0.9678462, llr_overestimation(llrs, y, seed=42))
+    np.testing.assert_almost_equal(0.9678462, llr_overestimation(lrs, y, seed=42))
 
     # big exaggerating system
     rng = np.random.default_rng(42)
     lr_h1 = np.exp(rng.normal(+lnlr_mean, lnlr_sigma, 1000) * 3)
     lr_h2 = np.exp(rng.normal(-lnlr_mean, lnlr_sigma, 1000) * 3)
-    llrs = np.log10(np.concatenate((lr_h1, lr_h2)))
+    lrs = np.concatenate((lr_h1, lr_h2))
     y = np.concatenate((np.ones(len(lr_h1)), np.zeros(len(lr_h2))))
-    np.testing.assert_almost_equal(2.1476174, llr_overestimation(llrs, y, seed=42))
+    np.testing.assert_almost_equal(2.1476174, llr_overestimation(lrs, y, seed=42))
 
 
 def test_llr_overestimation_interval():
@@ -50,9 +51,9 @@ def test_llr_overestimation_interval():
     rng = np.random.default_rng(42)
     lr_h1 = np.exp(rng.normal(+lnlr_mean, lnlr_sigma, 100))
     lr_h2 = np.exp(rng.normal(-lnlr_mean, lnlr_sigma, 100))
-    llrs = np.log10(np.concatenate((lr_h1, lr_h2)))
+    lrs = np.concatenate((lr_h1, lr_h2))
     y = np.concatenate((np.ones(len(lr_h1)), np.zeros(len(lr_h2))))
-    llr_overestimation_interval = calc_llr_overestimation(llrs, y, seed=42)[2]
+    llr_overestimation_interval = calc_llr_overestimation(lrs, y, seed=42)[2]
     interval_width = llr_overestimation_interval[:,2] - llr_overestimation_interval[:,0]
     np.testing.assert_almost_equal(1.5305846, np.nanmean(np.abs(interval_width)))
 
@@ -60,8 +61,11 @@ def test_llr_overestimation_interval():
     rng = np.random.default_rng(42)
     lr_h1 = np.exp(rng.normal(+lnlr_mean, lnlr_sigma, 1000))
     lr_h2 = np.exp(rng.normal(-lnlr_mean, lnlr_sigma, 1000))
-    llrs = np.log10(np.concatenate((lr_h1, lr_h2)))
+    lrs = np.concatenate((lr_h1, lr_h2))
     y = np.concatenate((np.ones(len(lr_h1)), np.zeros(len(lr_h2))))
-    llr_overestimation_interval = calc_llr_overestimation(llrs, y, seed=42)[2]
+    llr_overestimation_interval = calc_llr_overestimation(lrs, y, seed=42)[2]
     interval_width = llr_overestimation_interval[:,2] - llr_overestimation_interval[:,0]
     np.testing.assert_almost_equal(0.7807411, np.nanmean(np.abs(interval_width)))
+
+if __name__ == '__main__':
+    unittest.main()

c5a5e413f10202e8fa739e1978f9a90a03c78fd1
Leen van der Ham <l.van.der.ham@nfi.nl>
2025-10-23 16:19:26 +0200

    calc_llr_overestimation now uses llrs as input instead of lrs; all tests passing.

--- tests/algorithms/llr_overestimation.py
+++ tests/algorithms/llr_overestimation.py
@@ -1,5 +1,4 @@
 import numpy as np
-import unittest
 
 from lir.algorithms.llr_overestimation import calc_llr_overestimation
 from lir.metrics.overestimation import llr_overestimation
@@ -14,33 +13,33 @@ def test_llr_overestimation_metric():
     rng = np.random.default_rng(42)
     lr_h1 = np.exp(rng.normal(+lnlr_mean, lnlr_sigma, 100))
     lr_h2 = np.exp(rng.normal(-lnlr_mean, lnlr_sigma, 100))
-    lrs = np.concatenate((lr_h1, lr_h2))
+    llrs = np.log10(np.concatenate((lr_h1, lr_h2)))
     y = np.concatenate((np.ones(len(lr_h1)), np.zeros(len(lr_h2))))
-    np.testing.assert_almost_equal(0.2473928, llr_overestimation(lrs, y, seed=42))
+    np.testing.assert_almost_equal(0.2473928, llr_overestimation(llrs, y, seed=42))
 
     # big perfect system
     rng = np.random.default_rng(42)
     lr_h1 = np.exp(rng.normal(+lnlr_mean, lnlr_sigma, 1000))
     lr_h2 = np.exp(rng.normal(-lnlr_mean, lnlr_sigma, 1000))
-    lrs = np.concatenate((lr_h1, lr_h2))
+    llrs = np.log10(np.concatenate((lr_h1, lr_h2)))
     y = np.concatenate((np.ones(len(lr_h1)), np.zeros(len(lr_h2))))
-    np.testing.assert_almost_equal(0.0736284, llr_overestimation(lrs, y, seed=42))
+    np.testing.assert_almost_equal(0.0736284, llr_overestimation(llrs, y, seed=42))
 
     # big shifted system
     rng = np.random.default_rng(42)
     lr_h1 = np.exp(rng.normal(+lnlr_mean, lnlr_sigma, 1000) + np.log(10 ** 1))
     lr_h2 = np.exp(rng.normal(-lnlr_mean, lnlr_sigma, 1000) + np.log(10 ** 1))
-    lrs = np.concatenate((lr_h1, lr_h2))
+    llrs = np.log10(np.concatenate((lr_h1, lr_h2)))
     y = np.concatenate((np.ones(len(lr_h1)), np.zeros(len(lr_h2))))
-    np.testing.assert_almost_equal(0.9678462, llr_overestimation(lrs, y, seed=42))
+    np.testing.assert_almost_equal(0.9678462, llr_overestimation(llrs, y, seed=42))
 
     # big exaggerating system
     rng = np.random.default_rng(42)
     lr_h1 = np.exp(rng.normal(+lnlr_mean, lnlr_sigma, 1000) * 3)
     lr_h2 = np.exp(rng.normal(-lnlr_mean, lnlr_sigma, 1000) * 3)
-    lrs = np.concatenate((lr_h1, lr_h2))
+    llrs = np.log10(np.concatenate((lr_h1, lr_h2)))
     y = np.concatenate((np.ones(len(lr_h1)), np.zeros(len(lr_h2))))
-    np.testing.assert_almost_equal(2.1476174, llr_overestimation(lrs, y, seed=42))
+    np.testing.assert_almost_equal(2.1476174, llr_overestimation(llrs, y, seed=42))
 
 
 def test_llr_overestimation_interval():
@@ -51,9 +50,9 @@ def test_llr_overestimation_interval():
     rng = np.random.default_rng(42)
     lr_h1 = np.exp(rng.normal(+lnlr_mean, lnlr_sigma, 100))
     lr_h2 = np.exp(rng.normal(-lnlr_mean, lnlr_sigma, 100))
-    lrs = np.concatenate((lr_h1, lr_h2))
+    llrs = np.log10(np.concatenate((lr_h1, lr_h2)))
     y = np.concatenate((np.ones(len(lr_h1)), np.zeros(len(lr_h2))))
-    llr_overestimation_interval = calc_llr_overestimation(lrs, y, seed=42)[2]
+    llr_overestimation_interval = calc_llr_overestimation(llrs, y, seed=42)[2]
     interval_width = llr_overestimation_interval[:,2] - llr_overestimation_interval[:,0]
     np.testing.assert_almost_equal(1.5305846, np.nanmean(np.abs(interval_width)))
 
@@ -61,11 +60,8 @@ def test_llr_overestimation_interval():
     rng = np.random.default_rng(42)
     lr_h1 = np.exp(rng.normal(+lnlr_mean, lnlr_sigma, 1000))
     lr_h2 = np.exp(rng.normal(-lnlr_mean, lnlr_sigma, 1000))
-    lrs = np.concatenate((lr_h1, lr_h2))
+    llrs = np.log10(np.concatenate((lr_h1, lr_h2)))
     y = np.concatenate((np.ones(len(lr_h1)), np.zeros(len(lr_h2))))
-    llr_overestimation_interval = calc_llr_overestimation(lrs, y, seed=42)[2]
+    llr_overestimation_interval = calc_llr_overestimation(llrs, y, seed=42)[2]
     interval_width = llr_overestimation_interval[:,2] - llr_overestimation_interval[:,0]
     np.testing.assert_almost_equal(0.7807411, np.nanmean(np.abs(interval_width)))
-
-if __name__ == '__main__':
-    unittest.main()

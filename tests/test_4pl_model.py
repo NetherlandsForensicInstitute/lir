4c8a69e0edaa9a66b2386d580733bb29abb29254
wowtor <20113294+wowtor@users.noreply.github.com>
2025-10-27 11:34:14 +0100

    metrics take llrs as arguments, not lrs (#6)
    Co-authored-by: Leen van der Ham <l.van.der.ham@nfi.nl>
    Co-authored-by: wauter <75115764+LvdHam@users.noreply.github.com>

--- tests/test_4pl_model.py
+++ tests/test_4pl_model.py
@@ -7,12 +7,7 @@ from sklearn.linear_model import LogisticRegression
 
 from lir.algorithms.logistic_regression import FourParameterLogisticCalibrator
 from lir.metrics.devpav import devpav
-from lir.util import (
-    odds_to_probability,
-    Xn_to_Xy,
-    probability_to_logodds,
-    logodds_to_odds,
-)
+from lir.util import odds_to_probability, Xn_to_Xy, probability_to_logodds, logodds_to_odds
 
 
 def read_data(path):
@@ -25,14 +20,8 @@ def read_data(path):
 
 class TestFourParameterLogisticCalibrator(unittest.TestCase):
     dirname = os.path.dirname(__file__)
-    X_diff = read_data(
-        os.path.join(
-            dirname, "resources/LRsdifferentnormalLLRdistribmu_s=1N_ss=300.csv"
-        )
-    )
-    X_same = read_data(
-        os.path.join(dirname, "resources/LRssamenormalLLRdistribmu_s=1N_ss=300.csv")
-    )
+    X_diff = read_data(os.path.join(dirname, 'resources/LRsdifferentnormalLLRdistribmu_s=1N_ss=300.csv'))
+    X_same = read_data(os.path.join(dirname, 'resources/LRssamenormalLLRdistribmu_s=1N_ss=300.csv'))
 
     def test_compare_to_logistic(self):
         X, y = Xn_to_Xy(self.X_diff, self.X_same)
@@ -40,7 +29,7 @@ class TestFourParameterLogisticCalibrator(unittest.TestCase):
         four_pl_model = FourParameterLogisticCalibrator()
         four_pl_model.fit(X, y)
 
-        logistic = LogisticRegression(penalty=None, class_weight="balanced")
+        logistic = LogisticRegression(penalty=None, class_weight='balanced')
         logistic.fit(X[:, None], y)
         logistic_coef = [logistic.coef_[0][0], logistic.intercept_[0]]
         np.testing.assert_allclose(four_pl_model.coef_, logistic_coef, rtol=1e-2)
@@ -55,13 +44,11 @@ class TestFourParameterLogisticCalibrator(unittest.TestCase):
         four_pl_model.fit(X, y)
 
         logodds = four_pl_model.transform(X)
-        np.testing.assert_almost_equal(
-            devpav(logodds_to_odds(logodds), y), 0.12029952948152635, decimal=5
-        )
+        np.testing.assert_almost_equal(devpav(logodds, y), 0.12029952948152635, decimal=5)
 
     def test_pl_0_is_1(self):
-        X_same = np.concatenate([self.X_same, [1, 1 - 10**-10]])
-        X_diff = np.concatenate([self.X_diff, [1, 1 - 10**-10]])
+        X_same = np.concatenate([self.X_same, [1, 1 - 10 ** -10]])
+        X_diff = np.concatenate([self.X_diff, [1, 1 - 10 ** -10]])
         X, y = Xn_to_Xy(X_diff, X_same)
         X = probability_to_logodds(X)
 
@@ -69,13 +56,11 @@ class TestFourParameterLogisticCalibrator(unittest.TestCase):
         four_pl_model.fit(X, y)
 
         logodds = four_pl_model.transform(X)
-        np.testing.assert_almost_equal(
-            devpav(logodds_to_odds(logodds), y), 0.15273304557837525, decimal=5
-        )
+        np.testing.assert_almost_equal(devpav(logodds, y), 0.15273304557837525, decimal=5)
 
     def test_pl_0_is_1_and_pl_1_is_0(self):
-        X_same = np.concatenate([self.X_same, [0, 10**-10, 1, 1 - 10**-10]])
-        X_diff = np.concatenate([self.X_diff, [0, 10**-10, 1, 1 - 10**-10]])
+        X_same = np.concatenate([self.X_same, [0, 10 ** -10, 1, 1 - 10 ** -10]])
+        X_diff = np.concatenate([self.X_diff, [0, 10 ** -10, 1, 1 - 10 ** -10]])
         X, y = Xn_to_Xy(X_diff, X_same)
         X = probability_to_logodds(X)
 
@@ -83,10 +68,8 @@ class TestFourParameterLogisticCalibrator(unittest.TestCase):
         four_pl_model.fit(X, y)
 
         logodds = four_pl_model.transform(X)
-        np.testing.assert_almost_equal(
-            devpav(logodds_to_odds(logodds), y), 0.10475112893952891, decimal=5
-        )
+        np.testing.assert_almost_equal(devpav(logodds, y), 0.10475112893952891, decimal=5)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main()

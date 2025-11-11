790bd71fa0e246ec6f86937c38bf1e6c7020f5a6
Pim Meulensteen (DBS) <pim.meulensteen@nfi.nl>
2025-11-11 09:12:58 +0100

    Add a test for interval exptrapolation and use a fixture for the bootstrap tests.

--- tests/test_bootstrap.py
+++ tests/test_bootstrap.py
@@ -2,10 +2,62 @@ from sklearn.linear_model import LogisticRegression
 from lir.data.datasets.synthesized_normal_binary import SynthesizedNormalBinaryData, SynthesizedNormalDataClass
 from lir.algorithms.bootstraps import BootstrapAtData, BootstrapEquidistant
 import numpy as np
+import pytest
+from lir.data.models import FeatureData, LLRData
 
 
-def test_traindata_bootstrap():
+def test_traindata_bootstrap(sample_steps_and_data):
     """Test TrainDataBootstrap with a simple logistic regression model."""
+    steps, feature_data = sample_steps_and_data
+
+    model_data = BootstrapAtData(steps).fit(feature_data)
+    model_equidistant = BootstrapEquidistant(steps).fit(feature_data)
+
+    results_data = model_data.transform(feature_data)
+    results_equidistant = model_equidistant.transform(feature_data)
+
+    # Check that the llr values are within the inteval it has calculated.
+    assert np.all(results_data.llrs > results_data.llr_intervals[:, 0])
+    assert np.all(results_data.llrs < results_data.llr_intervals[:, 1])
+
+    assert np.all(results_equidistant.llrs > results_equidistant.llr_intervals[:, 0])
+    assert np.all(results_equidistant.llrs < results_equidistant.llr_intervals[:, 1])
+
+
+def test_interval_extrapolation(sample_steps_and_data):
+    """Test that the bootstrap intervals are reasonable."""
+    steps, feature_data = sample_steps_and_data
+
+    model_equidistant = BootstrapEquidistant(steps).fit(feature_data)
+
+    mn = np.min(feature_data.features)
+    mx = np.max(feature_data.features)
+
+    new_data = FeatureData(features=np.array([[mn - 1], [mn], [mx], [mx + 1]]))
+    results: LLRData = model_equidistant.transform(new_data)
+    # The difference between the interval at mn and mn-1 should be the same.
+    assert np.isclose(
+        results.llrs[0] - results.llr_intervals[0, 0],
+        results.llrs[1] - results.llr_intervals[1, 0],
+    )
+    assert np.isclose(
+        results.llrs[0] - results.llr_intervals[0, 1],
+        results.llrs[1] - results.llr_intervals[1, 1],
+    )
+
+    # This also holds for mx and mx+1.
+    assert np.isclose(
+        results.llrs[2] - results.llr_intervals[2, 0],
+        results.llrs[3] - results.llr_intervals[3, 0],
+    )
+    assert np.isclose(
+        results.llrs[2] - results.llr_intervals[2, 1],
+        results.llrs[3] - results.llr_intervals[3, 1],
+    )
+
+
+@pytest.fixture
+def sample_steps_and_data():
     data_spec = {
         0: SynthesizedNormalDataClass(-1, 1, 10),
         1: SynthesizedNormalDataClass(1, 1, 10),
@@ -16,16 +68,7 @@ def test_traindata_bootstrap():
     steps = [
         ('logreg', LogisticRegression(solver='lbfgs')),
     ]
-
-    results_data = BootstrapAtData(steps).fit(feature_data).transform(feature_data)
-    results_equidistant = BootstrapEquidistant(steps).fit(feature_data).transform(feature_data)
-
-    # Check that the llr values are within the inteval it has calculated.
-    assert np.all(results_data.llrs > results_data.llr_intervals[:, 0])
-    assert np.all(results_data.llrs < results_data.llr_intervals[:, 1])
-
-    assert np.all(results_equidistant.llrs > results_equidistant.llr_intervals[:, 0])
-    assert np.all(results_equidistant.llrs < results_equidistant.llr_intervals[:, 1])
+    return steps, feature_data
 
 
 def test_traindata_bootstrap_empty_pipeline():

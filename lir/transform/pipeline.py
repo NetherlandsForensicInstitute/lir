8311f6fa0952ddeff5744e9ce3a5d326e7d2e10a
wb <20113294+wowtor@users.noreply.github.com>
2025-11-13 13:11:44 +0100

    implemented debug pipeline

--- lir/transform/pipeline.py
+++ lir/transform/pipeline.py
@@ -1,10 +1,18 @@
+import logging
+from os import PathLike
 from pathlib import Path
 from typing import Any, Self
 
-from lir.config.base import ContextAwareDict, YamlParseError, check_is_empty, check_not_none, config_parser, pop_field
+import numpy as np
+
+from lir.config.base import ContextAwareDict, YamlParseError, check_is_empty, config_parser, pop_field
 from lir.config.transform import parse_module
+from lir.data.io import DataFileBuilderCsv
 from lir.data.models import FeatureData
 from lir.transform import Transformer, as_transformer
+
+
+LOG = logging.getLogger(__name__)
 
 
 class Pipeline(Transformer):
@@ -43,7 +51,9 @@ class Pipeline(Transformer):
         return instances
 
 
-def parse_steps(config: ContextAwareDict, output_dir: Path) -> list[tuple[str, Transformer]]:
+def parse_steps(config: ContextAwareDict | None, output_dir: Path) -> list[tuple[str, Transformer]]:
+    if config is None:
+        return []
     if not isinstance(config, ContextAwareDict):
         raise YamlParseError(config.context, f'invalid value for "steps": expected `dict`; found: {type(config)}')
     module_names = list(config.keys())
@@ -66,7 +76,94 @@ def pipeline(config: ContextAwareDict, output_dir: Path) -> Pipeline:
     if config is None:
         return Pipeline([])
 
-    steps = parse_steps(pop_field(config, 'steps', validate=check_not_none), output_dir)
+    steps = parse_steps(pop_field(config, 'steps'), output_dir)
 
     check_is_empty(config)
     return Pipeline(steps)
+
+
+class DebugPipeline(Pipeline):
+    """
+    A pipeline that writes debugging output to a CSV file.
+    """
+
+    def __init__(
+        self,
+        steps: list[tuple[str, Transformer | Any]],
+        output_file: PathLike,
+        include_batch_number: bool = True,
+        include_labels: bool = True,
+        include_fields: list[str] | None = None,
+        include_steps: list[str] | None = None,
+        include_input: bool = True,
+    ):
+        """
+        :param steps: the steps of the pipeline as a list of (name, module) tuples.
+        :param output_file: the name of the generated output file
+        :param include_batch_number: (bool) whether the zero-indexed batch number should be included in the output,
+            e.g. for cross-validation (defaults to True)
+        :param include_labels: (bool) whether the labels should be included, if available (defaults to True)
+        :param include_fields: a list of the names of extra fields to include (defaults to none)
+        :param include_steps: a list of steps to include (defaults to all)
+        :param include_input: (bool) whether to write the input features (defaults to True)
+        """
+        super().__init__(steps)
+        self.output_file = Path(output_file)
+        self.include_batch_number = include_batch_number
+        self.include_labels = include_labels
+        self.include_fields = include_fields or []
+        self.include_steps = set(include_steps or list(zip(*steps, strict=True))[0])
+        self.include_input = include_input
+        self.n_batches = 0
+
+    def transform(self, instances: FeatureData) -> FeatureData:
+        # initialize the csv builder
+        write_mode = 'w' if self.n_batches == 0 else 'a'
+        csv_builder = DataFileBuilderCsv(self.output_file, write_mode=write_mode)
+
+        # add column: batch
+        if self.include_batch_number:
+            csv_builder.add_column('batch', np.ones((len(instances), 1)) * self.n_batches)
+
+        # add column: label
+        if self.include_labels and instances.labels is not None:
+            csv_builder.add_column('label', instances.labels)
+
+        # add columns: features
+        if self.include_input:
+            csv_builder.add_column('features', instances.features)
+
+        try:
+            # add columns for the output of each step
+            for module_name, module in self.steps:
+                instances = module.transform(instances)
+
+                if module_name in self.include_steps:
+                    header = getattr(instances, 'header', None) or module_name
+                    csv_builder.add_column(header, instances.features)
+
+            # add columns for extra fields
+            for field in self.include_fields:
+                values = getattr(instances, field)
+                if not isinstance(values, np.ndarray):
+                    raise ValueError(f'expected type: np.ndarray; found: {type(values)}')
+                csv_builder.add_column(field, values)
+
+        finally:
+            # write all data that we accumulated, even if some steps failed
+            csv_builder.write()
+
+        self.n_batches += 1
+        return instances
+
+
+@config_parser
+def debug_pipeline(config: ContextAwareDict, output_dir: Path) -> Pipeline:
+    """Construct a scikit-learn Pipeline based on the provided configuration."""
+    if config is None:
+        return Pipeline([])
+
+    steps = parse_steps(pop_field(config, 'steps'), output_dir)
+    output_file = output_dir / pop_field(config, 'output_file', default=f'{config.context[-1]}.csv')
+
+    return DebugPipeline(steps, output_file, **config)

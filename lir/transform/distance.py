f53ab5f7f4ee2c5dacc61e8774de165db616cd7c
wauter <20113294+wowtor@users.noreply.github.com>
2026-01-09 14:56:39 +0100

    replaced AbsDiffTransformer by ElementWiseDifference and ManhattanDistance

--- lir/transform/distance.py
+++ lir/transform/distance.py
@@ -1,24 +1,43 @@
-from typing import Any, Self
-
 import numpy as np
-import sklearn
+
+from lir import Transformer
+from lir.data.models import FeatureData, InstanceData, PairedFeatureData
+from lir.util import check_type
 
 
-class AbsDiffTransformer(sklearn.base.TransformerMixin):
+class ElementWiseDifference(Transformer):
     """
     Takes an array of sample pairs and returns the element-wise absolute difference.
 
     Expects:
-        - X is of shape (n,f,2) with n=number of pairs; f=number of features; 2=number of samples per pair;
+        - a PairedFeatureData object with n_trace_instances=1 and n_ref_instances=1;
     Returns:
-        - X has shape (n, f)
+        - a copy of the FeatureData object with features of shape (n, f)
     """
 
-    def fit(self, X: Any, y: Any = None) -> Self:
-        return self
+    def transform(self, instances: InstanceData) -> FeatureData:
+        instances = check_type(PairedFeatureData, instances)
+        return instances.replace_as(
+            FeatureData, features=np.abs(instances.features[:, 0, :] - instances.features[:, 1, :])
+        )
 
-    def transform(self, X: np.ndarray) -> np.ndarray:
-        assert len(X.shape) == 3
-        assert X.shape[1] == 2
 
-        return np.abs(X[:, 0, :] - X[:, 1, :])
+class ManhattanDistance(Transformer):
+    """
+    Takes a PairedFeatureData object or a FeatureData object and returns the manhattan distance.
+
+    Expects:
+        - a FeatureData object; or
+        - a PairedFeatureData object with n_trace_instances=1, n_ref_instances=1
+    Returns:
+        - a FeatureData object with features of shape (n, 1)
+    """
+
+    def transform(self, instances: InstanceData) -> FeatureData:
+        instances = check_type(FeatureData, instances)
+        if isinstance(instances, PairedFeatureData):
+            instances = ElementWiseDifference().transform(instances)
+        if len(instances.features.shape) != 2:
+            raise ValueError(f'X must be of shape (n,f); found: {instances.features.shape}')
+
+        return instances.replace(features=np.sum(instances.features, axis=1))

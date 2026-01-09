eb086782d36ff52f4402a0280a3d3cbaa66270e3
John Braun <j.braun@nfi.nl>
2026-01-09 11:07:26 +0100

    Apply CI optimalization suggestions

--- tests/test_examples_yaml.py
+++ tests/test_examples_yaml.py
@@ -3,6 +3,7 @@ import shutil
 
 from pathlib import Path
 
+import pytest
 import confidence
 
 from lir.main import initialize_experiments
@@ -20,22 +21,34 @@ def test_parse_examples():
             raise ValueError(f"{yaml_file}: {e}")
 
 
+@pytest.mark.ci
 def test_run_examples():
     output_path = Path('tests/yaml_output')
 
+    # Potentially skip certain example YAML configurations
+    skipped_yaml_files = (
+        # 'optuna.yaml',
+    )
+
     # Start from a clean slate
-    shutil.rmtree(output_path); os.makedirs(output_path)
+    if output_path.exists():
+        shutil.rmtree(output_path)
+
+    os.makedirs(output_path)
 
     for i, yaml_file in enumerate(EXAMPLE_FILES):
-            configuration = confidence.Configuration(
-                confidence.loadf(yaml_file),
-                {'output_path': f'{output_path}/{i}'},
-            )
+        if yaml_file in skipped_yaml_files:
+            continue
 
-            experiments, _ = initialize_experiments(configuration)
+        configuration = confidence.Configuration(
+            confidence.loadf(yaml_file),
+            {'output_path': f'{output_path}/{i}'},
+        )
 
-            for name, experiment_definition in experiments.items():
-                try:
-                    experiment_definition.run()
-                except Exception as e:
-                    raise RuntimeError(f"Experiment '{name}' in '{yaml_file}' failed to run: {e}")
+        experiments, _ = initialize_experiments(configuration)
+
+        for name, experiment_definition in experiments.items():
+            try:
+                experiment_definition.run()
+            except Exception as e:
+                raise RuntimeError(f"Experiment '{name}' in '{yaml_file}' failed to run: {e}")

0303f7dd7ed8c899171a413f3ca4a1fa9dd7e3bb
Pim Meulensteen <pimmeulensteen+git@gmail.com>
2026-01-12 16:41:31 +0100

    Fix llr_overstimation test (#199)
    * Add test as prefix to llr_overstimation
    
    * fix llr_overestimation_test
    
    ---------
    
    Co-authored-by: Pim Meulensteen (DBS) <pim.meulensteen@nfi.nl>

--- lir/metrics/overestimation.py
+++ lir/metrics/overestimation.py
@@ -10,7 +10,7 @@ def llr_overestimation(llrs: np.ndarray, y: np.ndarray, **kwargs: Any) -> float:
     Calculates the mean absolute value of the LLR-overestimation.
     """
     _, llr_overestimation_grid, _ = calc_llr_overestimation(llrs, y, num_fids=0, **kwargs)
-    if llr_overestimation_grid and llr_overestimation_grid.all():
+    if llr_overestimation_grid is not None and llr_overestimation_grid.all():
         return float(np.mean(np.abs(llr_overestimation_grid)))
     else:
         return np.nan

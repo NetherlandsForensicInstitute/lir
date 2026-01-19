69fe9119b55b5451a4c4f7db04dde1a11e8b231e
John Braun <j.braun@nfi.nl>
2026-01-19 20:48:25 +0100

    Add Napoleon to allow Numpy style docstrings

--- docs/conf.py
+++ docs/conf.py
@@ -25,13 +25,30 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx_rtd_theme',
+    'sphinx.ext.napoleon',
 ]
+
 
 templates_path = ['_templates']
 exclude_patterns = []
 
 # Automatically generate stub pages for autosummary entries.
 autosummary_generate = True
+
+# Napoleon settings
+napoleon_google_docstring = True
+napoleon_numpy_docstring = True
+napoleon_include_init_with_doc = False
+napoleon_include_private_with_doc = False
+napoleon_include_special_with_doc = False
+napoleon_use_admonition_for_examples = False
+napoleon_use_admonition_for_notes = False
+napoleon_use_admonition_for_references = False
+napoleon_use_ivar = False
+napoleon_use_param = True
+napoleon_use_rtype = True
+napoleon_use_keyword = True
+napoleon_custom_sections = None
 
 
 # -- Options for HTML output -------------------------------------------------

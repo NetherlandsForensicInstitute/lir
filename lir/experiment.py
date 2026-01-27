2fe38f61f845c94993f7cbe1fb57ebb66e30a5cc
Pim Meulensteen <pimmeulensteen+git@gmail.com>
2026-01-27 15:32:33 +0100

    rename hyperparameter to lrsystem_parameter where relevant

--- lir/experiment.py
+++ lir/experiment.py
@@ -117,10 +117,10 @@ class PredefinedExperiment(Experiment):
         data_configs: list[tuple[ContextAwareDict, dict[str, Any]]],
         outputs: Sequence[Aggregation],
         output_path: Path,
-        lrsystems: list[tuple[ContextAwareDict, dict[str, Any]]],
+        lrsystem_configs: list[tuple[ContextAwareDict, dict[str, Any]]],
     ):
         super().__init__(name, outputs, output_path)
-        self.lrsystem_config = lrsystems
+        self.lrsystem_configs = lrsystem_configs
         self.data_configs = data_configs
 
     def _generate_and_run(self) -> None:
@@ -128,7 +128,7 @@ class PredefinedExperiment(Experiment):
         # there are multiple data configurations to evaluate.
         disable_data_tqdm = not lir.is_interactive() or len(self.data_configs) == 1
 
-        for data_config, dataparameter in tqdm(self.data_configs, disable=disable_data_tqdm, desc=self.name):
+        for data_config, data_parameter in tqdm(self.data_configs, disable=disable_data_tqdm, desc=self.name):
             # Parse the data configuration. This is done here to ensure that data
             # parsing is only done once per data configuration, even when multiple
             # LR systems are being evaluated on the same data setup.
@@ -137,21 +137,20 @@ class PredefinedExperiment(Experiment):
 
             # Only display the LR system configuration progress bar when running interactively, and
             # the data_tqdm is not disabled, and there are multiple LR systems to evaluate.
-            disable_lrsystem_tqdm = not lir.is_interactive() or not disable_data_tqdm or len(self.lrsystem_config) == 1
-            for lrsystem_config, hyperparameters in tqdm(
-                self.lrsystem_config, desc=self.name, disable=disable_lrsystem_tqdm
+            disable_lrsystem_tqdm = not lir.is_interactive() or not disable_data_tqdm or len(self.lrsystem_configs) == 1
+            for lrsystem_config, lrsystem_parameters in tqdm(
+                self.lrsystem_configs, desc=self.name, disable=disable_lrsystem_tqdm
             ):
-                # Combine the data parameter with the LR system hyperparameters to create
-                # a unique name for this experiment configuration.
-                data_name = '__'.join([f'{key}={value}' for key, value in dataparameter.items()])
-                lrsystem_name = '__'.join([f'{key}={value}' for key, value in hyperparameters.items()])
-                experiment_name = f'{data_name}{"__" if lrsystem_name and data_name else ""}{lrsystem_name}'
+                # Combine the data parameter with the LR system parameters to create a unique name for this run.
+                data_name = '__'.join([f'{key}={value}' for key, value in data_parameter.items()])
+                lrsystem_name = '__'.join([f'{key}={value}' for key, value in lrsystem_parameters.items()])
+                run_name = f'{data_name}{"__" if lrsystem_name and data_name else ""}{lrsystem_name}'
 
                 # This dictionary contains all parameters for this experiment run, prefixed
                 # by either 'data.' or 'lrsystem.' to avoid naming conflicts. This is used
                 # for reporting purposes in the aggregations.
-                parameters = {f'data.{k}': v for k, v in dataparameter.items()} | {
-                    f'lrsystem.{k}': v for k, v in hyperparameters.items()
+                parameters = {f'data.{k}': v for k, v in data_parameter.items()} | {
+                    f'lrsystem.{k}': v for k, v in lrsystem_parameters.items()
                 }
 
-                self._run_lrsystem(lrsystem_config, split_data, parameters, experiment_name, data_config)
+                self._run_lrsystem(lrsystem_config, split_data, parameters, run_name, data_config)

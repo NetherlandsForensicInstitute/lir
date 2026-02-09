5c37a98a4fb783f15746fbc404cc26246c100182
wb <20113294+wowtor@users.noreply.github.com>
2026-02-09 16:58:26 +0100

    cllr_min returns NaN on invalid input (like cllr)

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -34,6 +34,19 @@ def test_calculate_cllr(expected: float, h1_lrs: list[float], h2_lrs: list[float
 
 
 @pytest.mark.parametrize(
+    'expected,h1_lrs,h2_lrs',
+    [
+        (1, [1, 1], [1, 1]),
+    ],
+)
+def test_calculate_cllr_min(expected: float, h1_lrs: list[float], h2_lrs: list[float]):
+    lrs, labels = Xn_to_Xy(np.array(h1_lrs), np.array(h2_lrs))
+    llrs = odds_to_logodds(lrs)
+    llr_data = LLRData(features=llrs, labels=labels)
+    pytest.approx(expected, metrics.cllr_min(llr_data))
+
+
+@pytest.mark.parametrize(
     'expected,h1_llrs,h2_llrs',
     [
         (np.inf, [np.inf, 0], [0, 0]),
@@ -58,12 +71,17 @@ def test_extreme_cllr(expected: float, h1_llrs: list[float], h2_llrs: list[float
         ([np.nan, 0], [0, 0]),
         ([0, 0], [0, np.nan]),
         ([np.nan, np.nan], [np.nan, np.nan]),
+        ([0, 1, 2], []),
+        ([], [0, 1, 2]),
     ],
 )
 def test_illegal_cllr(h1_llrs, h2_llrs):
     llrs, labels = Xn_to_Xy(np.array(h1_llrs), np.array(h2_llrs))
     llr_data = LLRData(features=llrs, labels=labels)
     assert np.isnan(metrics.cllr(llr_data))
+
+    if np.all(np.isfinite(llrs)):  # this condition should be removed ?! --> see issue #301
+        assert np.isnan(metrics.cllr_min(llr_data))
 
 
 class TestDevPAV(unittest.TestCase):

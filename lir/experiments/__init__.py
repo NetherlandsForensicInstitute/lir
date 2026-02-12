7b24fd712b92f0ae36e6c27f07e879b58544ca8f
John Braun <j.braun@nfi.nl>
2026-02-12 22:00:40 +0100

    Move experiment strategies to dedicated experiments module
    In succession of the moved `optuna` experiment strategy, the other
    existing strategies have been moved to their own file in the
    `experiment` module.

---
+++ lir/experiments/__init__.py
@@ -0,0 +1,10 @@
+from lir.experiments.base_experiment import Experiment
+from lir.experiments.optuna import OptunaExperiment
+from lir.experiments.predefined import PredefinedExperiment
+
+
+__all__ = [
+    'Experiment',
+    'OptunaExperiment',
+    'PredefinedExperiment',
+]

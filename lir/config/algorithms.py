a82d16297c8ba80195f1d9226fd0d5b03defacb6
Pim Meulensteen <pimmeulensteen+git@gmail.com>
2026-03-03 09:26:59 +0100

    Add mcmc simulations from lr_module_scratch (#316)
    * Add pymc dependency / add mcmc simulations
    
    * fix mcmc / install deps / write test
    
    * Resolve github comments
    
    * Only use 'factory' internally / remove unwrapper

---
+++ lir/config/algorithms.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+from typing import Any
+
+from lir.algorithms.mcmc import McmcLLRModel
+from lir.config.base import ContextAwareDict, config_parser, pop_field
+from lir.config.transform import parse_module
+from lir.util import partial
+
+
+@config_parser
+def mcmc(config: ContextAwareDict, output_dir: Path) -> McmcLLRModel:
+    """Parse MCMC module config and resolve optional `bounding` to an `LLRBounder` instance."""
+    if 'bounding' in config:
+        bounding_config = pop_field(config, 'bounding')
+        if bounding_config is None:
+            config['bounding'] = None
+        else:
+            context = config.context + ['bounding'] if isinstance(bounding_config, str) else bounding_config.context
+            bounding = partial(parse_module, bounding_config, output_dir, context)
+            config['bounding'] = bounding
+
+    mcmc_class: Any = McmcLLRModel
+    return mcmc_class(**config)

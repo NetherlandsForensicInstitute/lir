c11467e9d3f67bf57db361869cb170b5ffa3dfaf
Pim Meulensteen <pimmeulensteen+git@gmail.com>
2026-03-12 13:52:52 +0100

    remove set_sources_for_plots

--- lir/lrsystems/lrsystems.py
+++ lir/lrsystems/lrsystems.py
@@ -12,22 +12,6 @@ LOG = logging.getLogger(__name__)
 class LRSystem(Transformer, ABC):
     """General representation of an LR system."""
 
-    def set_sources_for_plots(self, sources_for_plots: dict[str, str] | None) -> Self:
-        """Configure where scores should be extracted from.
-
-        Parameters
-        ----------
-        sources_for_plots : dict, optional
-            Pipeline step name to extract scores from (e.g., "scaler", "logistic_regression").
-
-        Returns
-        -------
-        Self
-            Returns self for method chaining.
-        """
-        self.sources_for_plots = sources_for_plots
-        return self
-
     def fit(self, instances: InstanceData) -> Self:
         """
         Fit the LR system on a set of features and corresponding labels.

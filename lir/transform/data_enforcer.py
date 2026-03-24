7d71ab7984af228a7948112d17ad293a19008ceb
Pim Meulensteen <pimmeulensteen+git@gmail.com>
2026-03-24 13:32:22 +0100

    implement PR comments

--- lir/transform/data_enforcer.py
+++ lir/transform/data_enforcer.py
@@ -6,16 +6,21 @@ from lir import FeatureData, InstanceData, Transformer
 from lir.transform import check_type
 
 
-class DataEnforcer(Transformer):
+class EnforceFeatureDataType(Transformer):
     """
     Module that enforces the data types of the features in the instances.
 
     This transformer is useful for ensuring that the data types of the features in the instances are consistent with the
     data types determined during fitting. This can help prevent errors during the application of a model/pipeline.
     Especially useful when the model or the applied data is read from file.
+
+    In short, it checks:
+    1. That the number of features in the instances matches the number of data types determined during fitting.
+    2. That the data types of the features can be cast to the data types determined during fitting.
     """
 
-    data_types: list[type]
+    _features_type: np.dtype
+    _features_size: int
 
     def fit(self, instances: InstanceData) -> Self:
         """
@@ -34,7 +39,10 @@ class DataEnforcer(Transformer):
             The fitted transformer.
         """
         instances = check_type(FeatureData, instances)
-        self.data_types = [type(instance) for instance in instances.features[0]]
+
+        self._features_type = instances.features.dtype
+        self._features_size = instances.features.shape[1]
+
         return self
 
     def apply(self, instances: InstanceData) -> FeatureData:
@@ -53,19 +61,23 @@ class DataEnforcer(Transformer):
         -------
         FeatureData
             The transformed data with enforced data types.
+
+        Raises
+        ------
+        ValueError
+            If the number of features in the instances does not match the number of data types determined during fitting
+        TypeError
+            If the data types of the features in the instances cannot be cast to the data type determined during fitting
         """
         instances = check_type(FeatureData, instances)
 
         # Ensrure that the number of features matches the number of data types determined during fitting.
-        if instances.features.shape[1] != len(self.data_types):
-            raise ValueError(f'Expected {len(self.data_types)} features but got {instances.features.shape[1]}')
+        if instances.features.shape[1] != self._features_size:
+            raise ValueError(f'Expected features of size {self._features_size} but got {instances.features.shape[1]}')
 
-        # Check that the data types of each feature in the instances match the data types determined during fitting.
         # Use np.can_cast to check if the value can be cast to the expected data type, which allows for some flexibility
         # in the data types (e.g., int can be cast to float, but not vice versa).
-        for instance in instances.features:
-            for value, data_type in zip(instance, self.data_types, strict=True):
-                if not np.can_cast(value, data_type):
-                    raise TypeError(f'Expected value of type {data_type} but got {type(value)} ({value}).')
+        if not np.can_cast(instances.features.dtype, self._features_type):
+            raise TypeError(f'Expected features of type {self._features_type} but got {instances.features.dtype}')
 
         return instances
--- tests/transform/test_data_enforcer.py
+++ tests/transform/test_data_enforcer.py
@@ -2,22 +2,22 @@ import numpy as np
 import pytest
 
 from lir import FeatureData
-from lir.transform.data_enforcer import DataEnforcer
+from lir.transform.data_enforcer import EnforceFeatureDataType
 
 
 def test_data_enforcer():
     features = np.array([[1, 2.0], [3, 4.0], [5, 6.0]], dtype=np.float16)
     instances = FeatureData(features=features)
 
-    data_enforcer = DataEnforcer()
+    data_enforcer = EnforceFeatureDataType()
     data_enforcer.fit(instances)
 
     # Apply the DataEnforcer to the same data (should pass)
     transformed_instances = data_enforcer.apply(instances)
     assert np.array_equal(transformed_instances.features, features)
 
-    # Apply a different set of features, but with the same data types (should pass)
-    features_different = np.array([[7, 8.0]], dtype=np.float16)
+    # Apply a different set of features, but with a castable data type (should pass)
+    features_different = np.array([[7, 8]], dtype=np.int8)
     instances_different = FeatureData(features=features_different)
     transformed_instances_different = data_enforcer.apply(instances_different)
     assert np.array_equal(transformed_instances_different.features, features_different)

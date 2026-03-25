f9e8efe8b406169feb0f026041a43185fa559fca
wb <20113294+wowtor@users.noreply.github.com>
2026-03-25 10:24:01 +0100

    removed NumpyWrapper class

--- lir/config/__init__.py
+++
@@ -1,5 +0,0 @@
-# This import is used to be able to write a short-hand version in `registry.yaml`, i.e.
-# `lir.config.numpy_csv_writer`. Ignored by linting (F401; unused import).
-from .transform import (  # noqa: F401
-    NumpyWrappingConfigParser as numpy_wrapper,  # noqa: N813
-)

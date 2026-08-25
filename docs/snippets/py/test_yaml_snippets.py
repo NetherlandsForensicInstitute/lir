8fc7158c710927cc133b1eb5f1050c123988431f
wb <20113294+wowtor@users.noreply.github.com>
2026-08-25 10:12:14 +0200

    docs: write a guide that describes how to extend LiR

---
+++ docs/snippets/py/test_yaml_snippets.py
@@ -0,0 +1,25 @@
+import sys
+import tempfile
+from pathlib import Path
+
+import pytest
+import requests_cache
+
+from tests.test_examples_yaml import run_yaml
+
+
+SNIPPET_DIR = Path(__file__).parent.parent
+SNIPPET_FILES = list(SNIPPET_DIR.rglob('*.yaml'))
+
+sys.path.append(str(Path(__file__).parent))
+
+
+@pytest.mark.parametrize('yaml_file', SNIPPET_FILES)
+def test_run_examples(yaml_file: Path):
+    listing_file = yaml_file.parent / 'resources' / yaml_file.with_suffix('.lst').name
+
+    with (
+        requests_cache.enabled(backend='filesystem', cache_name='tests/examples_yaml_resources/cache1'),
+        tempfile.TemporaryDirectory() as tmpdir,
+    ):
+        run_yaml(yaml_file, None, listing_file, Path(tmpdir))

f17cb1df10cd62ea29000bd93952514a53de2307
wb <20113294+wowtor@users.noreply.github.com>
2026-09-16 10:40:19 +0200

    aggregation.metrics_bars: add argument `yrange`

---
+++ tests/aggregation/test_metrics_bars.py
@@ -0,0 +1,37 @@
+from pathlib import Path
+
+import confidence
+import pytest
+
+from lir.aggregation.metrics_bars import parse
+from lir.config import ConfigValue, YamlParseError
+
+
+@pytest.mark.parametrize(
+    's,err',
+    [
+        (
+            """
+        metrics:
+          - cllr
+        yrange: [0, 1]
+        """,
+            None,
+        ),
+        (  # illegal argument: xrange
+            """
+        metrics:
+          - cllr
+        xrange: [0, 1]
+        """,
+            YamlParseError,
+        ),
+    ],
+)
+def test_parse(s: str, err: type[BaseException] | None):
+    cfg = ConfigValue.wrap([], confidence.loads(s))
+    if err:
+        with pytest.raises(err):
+            parse().parse(cfg, Path('/'))
+    else:
+        parse().parse(cfg, Path('/'))

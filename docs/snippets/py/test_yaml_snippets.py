16b938e21969573404c824bb93eebdf76f8c1716
wb <20113294+wowtor@users.noreply.github.com>
2026-09-24 10:56:03 +0200

    tests: use tmp_path fixture

--- docs/snippets/py/test_yaml_snippets.py
+++ docs/snippets/py/test_yaml_snippets.py
@@ -1,5 +1,4 @@
 import sys
-import tempfile
 from pathlib import Path
 
 import pytest
@@ -15,11 +14,8 @@ sys.path.append(str(Path(__file__).parent))
 
 
 @pytest.mark.parametrize('yaml_file', SNIPPET_FILES)
-def test_run_examples(yaml_file: Path):
+def test_run_examples(yaml_file: Path, tmp_path: Path):
     listing_file = yaml_file.parent / 'resources' / yaml_file.with_suffix('.lst').name
 
-    with (
-        requests_cache.enabled(backend='filesystem', cache_name='tests/examples_yaml_resources/cache'),
-        tempfile.TemporaryDirectory() as tmpdir,
-    ):
-        run_yaml(yaml_file, None, listing_file, Path(tmpdir))
+    with requests_cache.enabled(backend='filesystem', cache_name='tests/examples_yaml_resources/cache'):
+        run_yaml(yaml_file, None, listing_file, Path(tmp_path))

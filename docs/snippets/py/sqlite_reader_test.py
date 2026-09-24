9a9a6e71e86d1b5dd4358e2a95b42d0d9861fa88
wb <20113294+wowtor@users.noreply.github.com>
2026-09-24 11:12:53 +0200

    docs: close sqlite reader explicitly (which should be done automatically, but isn't?)

--- docs/snippets/py/sqlite_reader_test.py
+++ docs/snippets/py/sqlite_reader_test.py
@@ -11,6 +11,7 @@ def test_sqlite_reader(tmp_path: Path):
     with sqlite3.connect(filename) as db:
         db.execute('CREATE TABLE feature_table(hypothesis INT, feature1 FLOAT, feature2 FLOAT)')
         db.execute('INSERT INTO feature_table (hypothesis, feature1, feature2) VALUES (0, 1, 2), (0, 3, 4), (1, 5, 6)')
+    db.close()
 
     data = read_from_sqlite3(filename)
     assert np.all(data.features == np.array([1, 2, 3, 4, 5, 6]).reshape(-1, 2))
